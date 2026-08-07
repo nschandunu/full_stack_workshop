2b51a8eecf67b968fa8b32d7e2531a5723524f83
Senuka Chandunu <senukaacc@gmail.com>
2026-08-07 21:53:58 +0530

    chore: configure Prettier for project

--- server/src/app.js
+++ server/src/app.js
@@ -1,15 +1,15 @@
-const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/api/health", (req, res) => {
+app.get('/api/health', (req, res) => {
   res.json({
-    message: "API is running 🚀"
+    message: 'API is running 🚀',
   });
 });
 
