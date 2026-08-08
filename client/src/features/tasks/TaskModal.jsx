0209b091aaf7b2ec24fc3f1c306d470df6fee896
Oshada Rashmika <oshadar.rodrigo@gmail.com>
2026-08-08 23:48:54 +0530

    feat: add modal overlay and centered content shell

--- client/src/features/tasks/TaskModal.jsx
+++ client/src/features/tasks/TaskModal.jsx
@@ -1,4 +1,6 @@
 // @ts-check
+// @ts-ignore
+import styles from "./TaskModal.module.css";
 
 /**
  * @typedef {import("../../lib/types.jsx").Task} Task
@@ -9,11 +11,16 @@
  * @param {Task | null} props.task
  * @param {() => void} props.onClose
  */
-
 export default function TaskModal({ task, onClose }) {
   if (!task) {
     return null;
   }
 
-  return <div>{task.title}</div>;
+  return (
+    <div className={styles.overlay} onClick={onClose}>
+      <div className={styles.modal} onClick={(e) => e.stopPropagation()}>
+        {task.title}
+      </div>
+    </div>
+  );
 }
