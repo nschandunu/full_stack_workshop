837fa7db91b2b47bb0f8bf0874425c7ec98b0ed7
H I T L E R <ranasingheasila@gmail.com>
2026-08-27 00:22:26 +0530

    refactor: replace boards.js with boardRoutes.js, wire up controller endpoints with verifyToken middleware

--- collabboard-backend/routes/boardRoutes.js
+++ collabboard-backend/routes/boardRoutes.js
@@ -1,6 +1,23 @@
 const express = require('express');
+
 const router = express.Router();
 
-// Member 3 will implement board/column endpoints here
+const {
+  getBoards,
+  createBoard,
+  updateBoard,
+  deleteBoard,
+  addColumn
+} = require('../controllers/boardController');
+
+const verifyToken = require('../middleware/verifyToken');
+
+router.use(verifyToken);
+
+router.get('/', getBoards);
+router.post('/', createBoard);
+router.put('/:id', updateBoard);
+router.delete('/:id', deleteBoard);
+router.post('/:id/columns', addColumn);
 
 module.exports = router;
