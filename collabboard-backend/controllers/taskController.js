01b6de61533ec42740509540db29d50543c6620c
Oshada Rashmika <oshadar.rodrigo@gmail.com>
2026-08-26 23:13:52 +0530

    feat(tasks): implement GET /api/tasks route for collection retrieval

--- collabboard-backend/controllers/taskController.js
+++ collabboard-backend/controllers/taskController.js
@@ -1,1 +1,15 @@
-module.exports = {};
+const Task = require('../models/Task');
+
+const getTasks = async (req, res, next) => {
+  try {
+    const { boardId, columnId } = req.query;
+    const tasks = Task.findAll({ boardId, columnId });
+    res.status(200).json(tasks);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  getTasks,
+};
